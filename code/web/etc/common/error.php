100a9f0a69b89297bc15e494ebd0f569df8462c8
B00TK1D <stearns.josiah@gmail.com>
2022-02-19 16:55:24 -0700

    Fixed error function

--- code/web/etc/common/error.php
+++ code/web/etc/common/error.php
@@ -4,9 +4,6 @@
 
     function error($code = 0, $displayError = true) {
         if ($displayError) {
-            if (is_string($code)) {
-                $code = code($code);
-            }
             redirect("/utils/error?code=" . $code);
         }
         die();
