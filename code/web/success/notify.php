ef756212a06af81dfdd235c172ec744c12bd0d83
B00TK1D <stearns.josiah@gmail.com>
2022-04-18 09:12:16 -0600

    typo

--- code/web/success/notify.php
+++ code/web/success/notify.php
@@ -2,7 +2,7 @@
 
   include_once($_SERVER["DOCUMENT_ROOT"] . "/functs.php");
 
-  $machineID = param("machineID");
+  $machineID = param("machineID", ["sticky" => true]);
 
   $load = readObject("loads", ["machineID" => $machineID], 1);
 
