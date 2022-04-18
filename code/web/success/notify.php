c251d3006ba65f2e1bb923072afd79a8bc4d380a
B00TK1D <stearns.josiah@gmail.com>
2022-04-18 10:36:49 -0600

    Added time ago

--- code/web/success/notify.php
+++ code/web/success/notify.php
@@ -41,7 +41,7 @@
   </head>
   <body class="body">
     <div class="div-block">
-      <h1>This laundry was loaded by <br /><?php print($user["name"] . " " . time_elapsed_string(strtotime($load["created"]))); ?><br /> minutes ago</h1>
+      <h1>This laundry was loaded by <br /><?php print($user["name"] . " " . time_elapsed_string($load["created"])); ?><br /> minutes ago</h1>
       <a href="/load.php?machineID=<?php print($machineID); ?>" class="submit-button w-button">Back</a>
     </div>
     <script src="/jquery.js" type="text/javascript"></script>
