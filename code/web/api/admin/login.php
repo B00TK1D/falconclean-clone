474ee429ea2516a09c645e43684f2d4e73a71bfe
B00TK1D <stearns.josiah@gmail.com>
2022-04-18 09:42:21 -0600

    typo

--- code/web/api/admin/login.php
+++ code/web/api/admin/login.php
@@ -1,10 +1,15 @@
 <?php
     include_once($_SERVER["DOCUMENT_ROOT"] . "/functs.php");
 
+    $redirect = param("redirect");
+
     $password = param("password");
     if ($password == config("security.password")) {
         setSession("admin", true);
     }
 
+    if ($redirect) {
+        redirect($redirect);
+    }
     redirect("/admin/dashboard.php");
 ?>
