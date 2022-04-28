430917d59f7e52536a0f0d0183c80f003f3eddcd
B00TK1D <stearns.josiah@gmail.com>
2022-04-28 11:15:29 -0600

    Fixed admin stats

--- code/web/api/admin/login.php
+++ code/web/api/admin/login.php
@@ -1,15 +1,10 @@
 <?php
     include_once($_SERVER["DOCUMENT_ROOT"] . "/functs.php");
 
-    $redirect = param("redirect");
-
     $password = param("password");
     if ($password == config("security.password")) {
         setSession("admin", true);
     }
     
-    if ($redirect != "") {
-        redirect($redirect);
-    }
     redirect("/admin/dashboard.php");
 ?>
